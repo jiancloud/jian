108302c730bc49c22d23c7b1dd78a11a42b46d57
yangjian <642304396@qq.com>
2019-05-23 23:32:21 +0800

    Update UserInfo.java
    增加用户属性

--- src/main/java/com/cloud/jian/core/controller/UserInfo.java
+++ src/main/java/com/cloud/jian/core/controller/UserInfo.java
@@ -3,12 +3,13 @@ package com.cloud.jian.core.controller;
 import lombok.Data;
 import lombok.extern.log4j.Log4j2;
 
-@Log4j2
 @Data
 public class UserInfo {
 
     private String name;
     private String password;
-
+    private Integer age;
+    private String idCard;
+    private String nickName;
 
 }
